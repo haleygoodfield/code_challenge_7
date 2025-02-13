a13577fef2c37d087022ab9d8956ba86f3d6db29
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-13 13:16:59 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -16,5 +16,27 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
     let hourlyWage = salary / (hoursPerWeek * 52) // Formula
     return `Hourly Wage: $${hourlyWage.toFixed(2)}`; // Log using template literals
 };
+
 console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
 console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
+
+
+
+// Task 3: Arrow Function 
+// Write an arrow function calculateLoyaltyDiscount(amount, years) that applies a discount
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discountRate;
+    if (years >= 5) {
+        discountRate = 0.15; // 15% discount
+    } else if (years >= 3) {
+        discountRate =0.10; // 10% discount
+    } else {
+        discountRate = 0.05; // 5% discount
+    }
+    let discountPrice = amount * (1 - discountRate);
+    return `Discounted Price: $${discountPrice.toFixed(2)}`;
+};
+
+console.log(calculateLoyaltyDiscount(100, 6)); // Expected output: "Discounted Price: $85.00"
+console.log(calculateLoyaltyDiscount(200, 2)); // Expected output: "Discounted Price: $190.00"
+
