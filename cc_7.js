964d79a5f6543145ac32fd7bb0c71f0614b65dc2
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-15 11:19:10 -0500

    Task 1: Lines 5-9 log change

--- cc_7.js
+++ cc_7.js
@@ -2,11 +2,11 @@
 // Write a function calculateInvoice(subtotal, taxRate, discount) that calculates the final invoice amount
 function calculateInvoice(subtotal, taxRate, discount) {
     let total = (subtotal + (subtotal * taxRate)) - discount; // Formula
-    return `Total Invoice: $${total.toFixed(2)}`; // Log using template literals
+    console.log(`Total Invoice: $${total.toFixed(2)}`); // Log using template literals
 };
-
-console.log(calculateInvoice(100, 0.08, 5)); // Expected output: "Total Invoice: $103.00"
-console.log(calculateInvoice(500, 0.1, 20)); // Expected output: "Total Invoice: $530.00"
+// Test Cases
+calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
+calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"
 
 
 
